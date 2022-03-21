9ee9d16a0c6dc290af9628641d918776caf98c4c
kyu <2066apple@naver.com>
2022-03-21 15:24:13 +0900

    Feat: 글로벌 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <Routes/>
     </div>
   );
